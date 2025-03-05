afe2b43f7bff19c2e0a6fe02b5a725d19b5c1fa8
shion4949 <wshitou456@gmail.com>
2025-03-05 15:15:33 +0900

    microCMS組み込んだ

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.microcms-assets.io",
+            },
+        ],
+    },
+    env: {
+        MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+        MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+      },
+};
 
 export default nextConfig;
