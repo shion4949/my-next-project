86bfe9968d9118502a19f2b7a537a2b93ce4503d
shion4949 <wshitou456@gmail.com>
2025-07-06 14:57:35 +0900

    ニュースページできた

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import { getNewsDetail } from "@/app/_libs/microcms";
+import Article from "@/app/_components/Article";
+import styles from "./page.module.css";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { notFound } from "next/navigation";
 
-type Props = {
+/*type Props = {
   params: {
     slug: string;
   };
@@ -11,4 +15,27 @@ export default async function Page({ params}: Props) {
   const data = await getNewsDetail(params.slug);
 
   return<div>{data.title}</div>
+}*/
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const slug = params.slug; // ★ ここで一旦変数に取り出す
+  console.log("params.slug:", slug);
+
+  const data = await getNewsDetail(slug).catch(notFound);
+
+  return(
+    <>
+    <Article data = {data} />
+    <div className={styles.footer}>
+      <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+    </div>
+    </>
+  );
 }
+
